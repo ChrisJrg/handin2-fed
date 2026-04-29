ec9aa874d385acf54554acfbf99487d58b413ebf
Mikkel Kristiansen <202301563@post.au.dk>
2026-04-29 10:14:08 +0200

    Navigation is complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import {Routes, Route, Navigate} from "react-router-dom";
+import {Routes, Route, Navigate, NavLink} from "react-router-dom";
 import CreateManager from "./pages/Managers/CreateManager.jsx"
 import CreateModel from "./pages/Models/CreateModel.jsx"
 import ControlledForm from "./pages/Login.jsx"
@@ -9,19 +9,56 @@ import MyJobs from "./pages/Models/MyJobs.jsx"
 export function App() {
 
   return (
-    <>
-    <Routes >
-        <Route path="/" element={<ControlledForm/>}></Route>
+      <>
+        <nav className={"navbar"}>
 
-        <Route path="/jobs" element={<CreateManager/>} />
-        <Route path="/my-jobs" element={<MyJobs/>} />
-        <Route path="/create-models" element={<CreateModel/>} />
+            <NavLink
+                to={"/jobs"}
+                className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}
+            >
+                Jobs
+            </NavLink>
 
+            <NavLink
+                to={"/my-jobs"}
+                className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}
+            >
+                My Jobs
+            </NavLink>
 
+            <NavLink
+                to={"/create-models"}
+                className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}
+            >
+                Create a Model
+            </NavLink>
 
-        <Route path="*" element={<Navigate to="/" />} />
-    </Routes>
-    </>
+            <NavLink
+                to={"/create-managers"}
+                className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}
+            >
+                Create a Manager
+            </NavLink>
+
+            <NavLink
+                to={"/"}
+                className={({ isActive }) => isActive ? "nav-link-logOut active" : "nav-link-logOut"}
+            >
+                Logout
+            </NavLink>
+
+        </nav>
+          <Routes>
+              <Route path="/" element={<ControlledForm/>}></Route>
+
+              <Route path="/jobs" element={<CreateManager/>}/>
+              <Route path="/my-jobs" element={<MyJobs/>}/>
+              <Route path="/create-models" element={<CreateModel/>}/>
+              <Route path="/create-managers" element={<CreateManager/>}/>
+
+              <Route path="*" element={<Navigate to="/"/>}/>
+          </Routes>
+      </>
   )
 }
 
