f376c6e4b6d0ce7ee081ed0403140f4c3f1e88f7
Christopher F. Jørgensen <202300791@post.au.dk>
2026-04-30 15:10:45 +0200

    created route to specific job

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import ControlledForm from "./Pages/Login.jsx";
 import CreateManager from "./Pages/Managers/CreateManager.jsx";
 import MyJobs from "./Pages/Models/MyJobs.jsx";
 import CreateModel from "./Pages/Models/CreateModel.jsx";
+import ModelJob from "./Pages/Models/MyJob.jsx";
 
 
 export function App() {
@@ -19,6 +20,7 @@ export function App() {
               <Route path="/my-jobs" element={<MyJobs/>}/>
               <Route path="/create-models" element={<CreateModel/>}/>
               <Route path="/create-managers" element={<CreateManager/>}/>
+              <Route path="/my-jobs/:jobId" element={<ModelJob/>}/>
 
               <Route path="*" element={<Navigate to="/"/>}/>
           </Routes>
--- src/Pages/Models/MyJobs.jsx
+++ src/Pages/Models/MyJobs.jsx
@@ -1,6 +1,7 @@
 import "../../Styles/MyJobs.css"
 import {useEffect, useState} from "react";
 import edit_png from "../../assets/pencil-edit-button.svg";
+import {Link} from "react-router-dom";
 
 
 export function MyJobs(){
@@ -34,12 +35,17 @@ export function MyJobs(){
                 <section id="job-container-outer"  key={job.jobId}>
                     <section id="job-container">
                     <text>Customer: {job.customer}</text>
-                    <text>Start date: {job.startDate}</text>
+                    <text>Start date: {job.startDate.split("T")[0]}</text>
                     <text>Duration in days: {job.days}</text>
                     <text>Location: {job.location}</text>
                     <text>Extra information: {job.comments}</text>
                     </section>
-                    <button><img id="edit" src={edit_png} alt="edit" /></button>
+
+                    <nav>
+                        <Link to={`/my-jobs/:${job.jobId}`}>
+                    <button ><img id="edit" src={edit_png} alt="edit" /></button>
+                        </Link>
+                    </nav>
                 </section>
             ))}
 
